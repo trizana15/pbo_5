693849b0f310085505364c40f6eb3bb48a1339fd
TrizanaWafi258 <trizanaresfara@gmail.com>
2024-10-21 11:14:16 +0700

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class App {
     @SuppressWarnings("resource")
     public static void main(String[] args) throws Exception {
@@ -7,26 +6,27 @@ public class App {
         //variable "cashier" --> instance of class Cashier 
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            System.out.println("Program Sistem Kasir: ");
             System.out.println("1. Tambah Barang");
             System.out.println("2. Hapus Barang");
             System.out.println("3. Tampilkan Total");
             System.out.println("4. Keluar");
-            System.out.print("Pilih opsi: ");
+            System.out.print("Pilih: ");
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
                     System.out.print("Masukkan kode barang: ");
-                    String addCode = scanner.next();
+                    String addKode = scanner.next();
                     System.out.print("Masukkan nama barang: ");
-                    String addName = scanner.next();
+                    String addNama = scanner.next();
                     System.out.print("Masukkan harga barang: ");
-                    double addPrice = scanner.nextDouble();
-                    cashier.addItem(addPrice, addCode, addName);
+                    double addHarga = scanner.nextDouble();
+                    cashier.addItem(addHarga, addKode, addNama);
                     break;
                 case 2:
                     System.out.print("Masukkan kode barang yang akan dihapus: ");
-                    String removeCode = scanner.next();
-                    cashier.removeItem(removeCode);
+                    String removeKode = scanner.next();
+                    cashier.removeItem(removeKode);
                     break;
                 case 3:
                     cashier.displayTotal();
@@ -35,8 +35,8 @@ public class App {
                     System.out.println("Terima kasih telah menggunakan sistem kasir kami!");
                     return;
                 default:
-                    System.out.println("Opsi tidak valid.");
+                    System.out.println("Pilihan tidak valid.");
             }
-        }
+        
     }
 }
