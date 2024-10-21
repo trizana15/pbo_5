f2395b28a39c2aa7f8ed4cc7e92460331b2ef116
TrizanaWafi258 <trizanaresfara@gmail.com>
2024-10-21 11:15:58 +0700

    Update Cashier.java

--- Cashier.java
+++ Cashier.java
@@ -1,20 +1,16 @@
 import java.util.ArrayList;
-
 public class Cashier {
     private ArrayList<Item> items;
-
     public Cashier() {
         items = new ArrayList<>();
     }
-
     public void addItem(double p, String c, String n) {
         items.add(new Item(c, n, p));
         System.out.println("Barang ditambahkan.");
     }
-
     public void removeItem(String code) {
         for (Item item : items) {
-            if (item.getCode() == code) {
+            if (item.getKode() == kode) {
                 items.remove(item);
                 System.out.println("Barang dihapus.");
                 return;
@@ -22,7 +18,6 @@ public class Cashier {
         }
         System.out.println("Barang tidak ditemukan.");
     }
-
     public void displayTotal() {
         double total = 0.0;
         for (Item item : items) {
